9ebf02f5a35c469e9edc2dbacd1100e35d08d0c2
Nitin <nitin.saraswat@remotestate.com>
2023-02-28 15:15:39 +0530

    Assignment 2 Started

---
+++ AssignmentXML/AssignmentXML/DAL/Entities/XmlTemplate.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AssignmentXML.DAL.Entities
+{
+    [Table("tblXmlTemplate")]
+    public class XmlTemplate
+    {
+        [Key]
+        [Required] 
+        public string Code { get; set; }
+
+        [Required]
+        public string Xml { get; set; }
+
+        [Required]
+        public bool isDeleted { get; set; }
+
+        public int id { get; }
+    }
+
+}
