7855df133ea2eea29614b53693c1bb966098154d
Nitin <nitin.saraswat@remotestate.com>
2023-03-06 11:08:29 +0530

    xml update

---
+++ Challenge 01/Challenge 01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //    Console.WriteLine("Hello World");
+            //    Console.ReadKey();
+
+            var arr = new int[5];
+            arr[2] = 12;
+            arr[3] = 12;
+            arr[4] = 12;
+            arr[5] = 12;
+
+
+            for (int i = 0; i < arr.Length; i++ >{
+                Console.WriteLine(arr[i]);
+            }
+
+                Console.ReadKey());
+        }
+    }
+}
